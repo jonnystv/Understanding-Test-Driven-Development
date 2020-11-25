5a121580b0cb58022ea4b608e237fc3fef77e661
jonnystv <jonathan@darefoundation.com>
2020-11-25 22:41:03 +0000

    coding task completed - 2

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -1,5 +1,5 @@
 def compare(num_1, num_2):
-    
+
     if num_1 < num_2:
         return f"{num_1} is less than {num_2}"
 
@@ -7,3 +7,5 @@ def compare(num_1, num_2):
         return f"{num_1} is greater than {num_2}"
 
     return f"{num_1} is equal to {num_2}"
+
+    #Didn't look at the solution before writing the above so I accidentally skipped a step!
